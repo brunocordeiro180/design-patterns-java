aae290598f32b98b45ae790d6ef78c8796e61d44
Bruno Cordeiro Mendes <brunocordeiro180@gmail.com>
2022-03-12 15:59:35 -0300

    Remove unnecessary Override annotation

--- strategy/com/strategy/NotificacaoDiretorio.java
+++ strategy/com/strategy/NotificacaoDiretorio.java
@@ -2,7 +2,6 @@ package strategy.com.strategy;
 
 public class NotificacaoDiretorio implements NotificacaoStrategy {
     
-    @Override
     public void gerarNotificacao() {
         System.out.println("Gerada notificacao de diretorio");
     }
--- strategy/com/strategy/NotificacaoMovimento.java
+++ strategy/com/strategy/NotificacaoMovimento.java
@@ -2,7 +2,6 @@ package strategy.com.strategy;
 
 public class NotificacaoMovimento implements NotificacaoStrategy {
     
-    @Override
     public void gerarNotificacao() {
         System.out.println("Gerada notificacao de movimento");
     }
